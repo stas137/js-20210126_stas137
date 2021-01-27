78e308fde1516d151e28b9720dc2174f835de825
stas137 <stasccb@gmail.com>
2021-01-27 14:41:33 +0300

    tast 2.1

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,30 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
+    //let param = 'asc';
+    
+    //let arr = ['banana', 'Banana', 'яблоко', 'Яблоко', 'ёжик', 'Ёжик'];
+    //console.log(arr);
 
+    let new_arr = arr.map(item => item);
+    //let new_arr = arr.slice(0, arr.length);
+
+    new_arr.sort(function (str, str2){
+        let collator = new Intl.Collator(['ru-RU', 'en-EN'], { caseFirst: 'upper' });
+        return collator.compare(str, str2);
+        //return str.localeCompare(str2);
+    });
+
+    if (param == 'asc'){
+        //console.log(new_arr);
+        return new_arr;
+    }
+    else {
+        new_arr.reverse();
+        //console.log(new_arr);
+        return new_arr;
+    }
+    
 }
+
+
