cdace2ccebe2ba5509cbaa71ac786cf43a1ab884
stas137 <stasccb@gmail.com>
2021-01-28 00:27:54 +0300

    task 2.3

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -5,5 +5,32 @@
  * @returns {object} - returns the new object
  */
 export const omit = (obj, ...fields) => {
+    
+/*     const obj = {
+        apple: 2,
+        orange: 4,
+        banana: 3
+    };
+    let copy_fields = ['apple', 'banana']; */
 
+    let copy_obj = Object.entries(obj);
+    let copy_fields = [...fields]; 
+
+    let res_obj = {};
+    let flag = false;
+
+    for (let item of copy_obj){
+        flag = false;
+        for (let item_f of copy_fields){
+            if (item[0] == item_f){
+                flag = true;
+                break;
+            }
+        } 
+        if (!flag) {
+            res_obj[item[0]]=item[1];
+        }
+    }
+    
+    return res_obj;
 };
